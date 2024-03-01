1afd9abdbc44923d19ca0d487c63d89d5ad6c043
kamol0007 <kamolbek00070007@gmail.com>
2024-03-02 00:22:19 +0500

    updated

--- src/styleComponents/GlobalStyle.jsx
+++ src/styleComponents/GlobalStyle.jsx
@@ -1,53 +1,8 @@
 import styled from "styled-components";
 
 export const Btn = styled.button`
-  background: #00aa58;
-  color: #ffffff;
   font-size: 20px;
-  font-style: normal;
-  font-weight: 700;
-  /* line-height: 27px;  */
-  letter-spacing: 1px;
-  text-align: center;
-  padding: 16px 50px;
-  border-radius: 10px;
-  border: 1px solid #00aa58;
-  cursor: pointer;
-  transition: background-color 0.5s ease;
-  &:hover {
-    opacity: 0.85;
-    background: #00aa58 !important;
-    color: #fff !important;
-    border: 1px solid #00aa58 !important;
-    /* background: #fff !important;
-    color: #00aa58 !important;
-    border: 1px solid #00aa58 !important; */
-  }
   @media (max-width: 576px) {
     font-size: 18px;
-    padding: 12px 30px;
-  }
-`;
-export const BtnWhite = styled.button`
-  border-radius: 10px;
-  border: 1px solid #00aa58;
-  background: #ffffff;
-  color: #00aa58;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 700;
-  /* line-height: 27px;  */
-  letter-spacing: 1px;
-  text-align: center;
-  padding: 16px 50px;
-  cursor: pointer;
-  &:hover {
-    /* opacity: 0.7; */
-    background: #00aa58;
-    color: #fff;
-  }
-  @media (max-width: 576px) {
-    font-size: 18px;
-    padding: 12px 30px;
   }
 `;
