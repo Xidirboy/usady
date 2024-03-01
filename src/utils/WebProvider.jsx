a65fb3cc176d5d543ab59865ddfa6c067be55915
kamol0007 <kamolbek00070007@gmail.com>
2024-03-02 00:10:38 +0500

    updated

--- src/utils/WebProvider.jsx
+++ src/utils/WebProvider.jsx
@@ -1,57 +1,18 @@
-import React, { Suspense, lazy, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Axios from "./httpClient";
-import { get } from "lodash";
+import React, { Suspense, lazy } from "react";
+import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
-// import LoadingMain from "../sections/utilsSections/LoadingMain";
-// import AudioDiktor from "../sections/utilsSections/header/AudioDiktor";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./i18n";
 const LoadingMain = lazy(() => import("../sections/utilsSections/LoadingMain"));
-const AudioDiktor = lazy(() =>
-  import("../sections/utilsSections/header/AudioDiktor")
-);
 
 const WebProvider = ({ children }) => {
   const { i18n } = useTranslation();
-  const dispatch = useDispatch();
-  const { loading, footer } = useSelector((s) => s);
-  useEffect(() => {
-    getMenu();
-    if (!footer?.links?.length) getFooter();
-  }, [i18n.language]);
-  const getMenu = () => {
-    dispatch({ type: "SET_MENU_LOADING", payload: true });
-    Axios()
-      .get(`api/v1/menu/list`)
-      .then((r) => {
-        dispatch({ type: "SET_MENU", payload: get(r, "data.data", []) });
-      })
-      .catch((e) => {})
-      .finally(() => {
-        dispatch({ type: "SET_MENU_LOADING", payload: false });
-      });
-  };
-  const getFooter = () => {
-    // dispatch({ type: "SET_MENU_LOADING", payload: true });
-    Axios()
-      .get(`api/v1/footer`)
-      .then((r) => {
-        dispatch({ type: "SET_FOOTER", payload: get(r, "data.data", {}) });
-      })
-      .catch((e) => {})
-      .finally(() => {
-        // dispatch({ type: "SET_MENU_LOADING", payload: false });
-      });
-  };
+  const { loading } = useSelector((s) => s);
   return (
     <>
       <ChakraProvider>
         {children}
-        <Suspense>
-          {loading && <LoadingMain />}
-          <AudioDiktor />
-        </Suspense>
+        <Suspense>{loading && <LoadingMain />}</Suspense>
       </ChakraProvider>
     </>
   );
--- src/utils/reducers.jsx
+++ src/utils/reducers.jsx
@@ -1,29 +1,12 @@
 // import _ from "lodash";
 
 const INITIAL_STATE = {
-  menu: [],
-  menu_loading: false,
   loading: false,
   user: {},
-  audio_diktor: false,
-  footer: {},
-  authModal: false,
-  sosModal: false,
 };
 
 const reducers = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_MENU":
-      return {
-        ...state,
-        menu: action?.payload,
-        menu_loading: false,
-      };
-    case "SET_MENU_LOADING":
-      return {
-        ...state,
-        menu_loading: action?.payload,
-      };
     case "SET_LOADING":
       return {
         ...state,
@@ -33,26 +16,6 @@ const reducers = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         user: action?.payload,
-      };
-    case "SET_AUDIO_DIKTOR":
-      return {
-        ...state,
-        audio_diktor: action?.payload,
-      };
-    case "SET_FOOTER":
-      return {
-        ...state,
-        footer: action?.payload,
-      };
-    case "SET_AUTH_MODAL":
-      return {
-        ...state,
-        authModal: action?.payload,
-      };
-    case "SET_SOS_MODAL":
-      return {
-        ...state,
-        sosModal: action?.payload,
       };
     default:
       return state;
