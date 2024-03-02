a7449f01a10798d0f5dd029c256d7ad7ce6595cb
kamol0007 <kamolbek00070007@gmail.com>
2024-03-02 14:32:21 +0500

    updated

---
+++ src/sections/layoutSections/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+const HeaderStyle = styled.div``;
+const Header = () => {
+  return <HeaderStyle>Header</HeaderStyle>;
+};
+
+export default Header;
---
+++ src/sections/layoutSections/Layout.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Header from "./Header";
+
+const LayoutStyle = styled.div``;
+const Layout = () => {
+  return (
+    <LayoutStyle>
+      <Header />
+      Layout
+    </LayoutStyle>
+  );
+};
+
+export default Layout;
