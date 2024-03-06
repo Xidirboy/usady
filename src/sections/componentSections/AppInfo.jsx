056611ac2f130e011559a12d1f9188913fd576a3
kamol0007 <kamolbek00070007@gmail.com>
2024-03-07 00:33:40 +0500

    updated

--- src/sections/componentSections/AppInfo.jsx
+++ src/sections/componentSections/AppInfo.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { planIcon } from "../../assets/homeS3Icon";
+import {
+  cashIcon,
+  planIcon,
+  starIcon,
+  usersIcon,
+} from "../../assets/homeS3Icon";
 const AppInfoStyle = styled.div`
   & .i_title {
     font-size: 26px;
@@ -24,6 +29,26 @@ const AppInfoStyle = styled.div`
       padding: 0 30px;
     }
   }
+  & .items {
+    justify-content: left;
+    flex-wrap: wrap;
+    margin: -5px;
+    & .item {
+      background: #ebf7ff;
+      padding: 20px;
+      border-radius: 10px;
+      font-size: 24px;
+      font-weight: 500;
+      line-height: 20px;
+      letter-spacing: 0.2px;
+      margin: 5px;
+      & svg {
+        width: 25px;
+        height: 25px;
+        margin-right: 10px;
+      }
+    }
+  }
 `;
 const AppInfo = () => {
   return (
@@ -33,6 +58,11 @@ const AppInfo = () => {
         <span>Узбекистан, ташкент</span>
         <span className="icon">{planIcon}</span>
         <span>Турция, стамбул</span>
+      </div>
+      <div className=" ds_flex items">
+        <div className="dc_flex item star">{starIcon}3-5</div>
+        <div className="dc_flex item">{usersIcon}2 взрослых</div>
+        <div className="dc_flex item">{cashIcon}15 000 000 сумм</div>
       </div>
     </AppInfoStyle>
   );
