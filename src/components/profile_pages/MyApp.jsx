72d9bec53f4dfec9400ff7f9d177c273d57c8ffa
kamol0007 <kamolbek00070007@gmail.com>
2024-03-06 23:20:48 +0500

    updated

--- src/components/profile_pages/MyApp.jsx
+++ src/components/profile_pages/MyApp.jsx
@@ -1,10 +1,35 @@
+import styled from "styled-components";
 import ShowTitle from "../../sections/utilsSections/ShowTitle";
-
+import AppInfo from "../../sections/componentSections/AppInfo";
+const MyAppStyle = styled.div`
+  & .apps {
+    padding-top: 30px;
+    padding-bottom: 50px;
+    & .app {
+      & .app_info {
+        border: 1px solid #e0e0e0;
+        background-color: #fff;
+        max-width: 1120px;
+        border-radius: 15.13px;
+      }
+    }
+  }
+`;
 const MyApp = () => {
   return (
-    <>
+    <MyAppStyle>
       <ShowTitle title="Мои заявки"></ShowTitle>
-    </>
+      <div className="container_main ">
+        <div className="apps">
+          <div className="app">
+            <div className="app_info">
+              <AppInfo />
+            </div>
+            <div className="app_v"></div>
+          </div>
+        </div>
+      </div>
+    </MyAppStyle>
   );
 };
 export default MyApp;
---
+++ src/sections/componentSections/AppInfo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+const AppInfoStyle = styled.div``;
+const AppInfo = () => {
+  return <AppInfoStyle>AppInfo</AppInfoStyle>;
+};
+
+export default AppInfo;
