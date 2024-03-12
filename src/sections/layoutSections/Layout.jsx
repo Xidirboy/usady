ad92c79ad4860a6d725383318cbf972f63434bb1
kamol0007 <kamolbek00070007@gmail.com>
2024-03-12 23:37:46 +0500

    updated

--- src/sections/layoutSections/Layout.jsx
+++ src/sections/layoutSections/Layout.jsx
@@ -1,8 +1,25 @@
 import styled from "styled-components";
 import Header from "./Header";
+import Axios from "../../utils/httpClient";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const LayoutStyle = styled.div``;
 const Layout = ({ children }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((s) => s);
+  useEffect(() => {
+    if (!user?.id) getUser();
+  }, []);
+  const getUser = () => {
+    Axios()
+      .get(`api/v1/auth/me`)
+      .then((r) => {
+        dispatch({ type: "SET_USER", payload: r?.data?.user ?? {} });
+      })
+      .catch((e) => {})
+      .finally(() => {});
+  };
   return (
     <LayoutStyle>
       <Header />
