53d10ec8b629d4f77cd5d99bb451a7231ec2f586
kamol0007 <kamolbek00070007@gmail.com>
2024-03-16 14:34:29 +0500

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,10 +14,19 @@ const HomeStyle = styled.div`
     #182e63 93.82%,
     #1e2344 109.5%
   );
+  @media (max-width: 900px) {
+    border-radius: 20px 20px 0 0;
+  }
   & .section_1 {
     min-height: 600px;
     align-items: stretch;
     padding-right: 0 !important;
+    @media (max-width: 900px) {
+      flex-direction: column;
+      align-items: center;
+      min-height: auto;
+      padding-left: 0 !important;
+    }
     & .s1 {
       max-width: 700px;
       & .title {
@@ -27,6 +36,13 @@ const HomeStyle = styled.div`
         letter-spacing: 0em;
         text-align: left;
         color: #fff;
+        @media (max-width: 900px) {
+          padding: 30px 20px;
+          font-size: 32px;
+          font-weight: 400;
+          line-height: 38px;
+          text-align: center;
+        }
       }
     }
     & .s2 {
@@ -34,6 +50,20 @@ const HomeStyle = styled.div`
       background-image: url("/images/home/s1.png");
       background-size: cover;
       background-position: left;
+      background-repeat: no-repeat;
+      border-radius: 0px 50px 0 0;
+      @media (max-width: 900px) {
+        height: 430px;
+        background-size: contain;
+        background-position: center;
+        border-radius: 0;
+      }
+      @media (max-width: 700px) {
+        height: 300px;
+      }
+      @media (max-width: 500px) {
+        background-size: cover;
+      }
     }
   }
   & .section_2 {
@@ -50,7 +80,8 @@ const Home = () => {
         <div className="ds_flex container_main section_1 ">
           <div className="ds_flex s1">
             <div className="title">
-              <span>100+</span> Турагентов готовы подобрать лучший тур
+              <span>100+</span> Турагентов
+              <br /> готовы подобрать лучший тур
             </div>
           </div>
           <div className="s2"></div>
