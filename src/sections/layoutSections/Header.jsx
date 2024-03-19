9352ead8d5b231a0bc5528fb41d7a623411ae071
kamol0007 <kamolbek00070007@gmail.com>
2024-03-20 01:07:18 +0500

    updated

--- src/sections/layoutSections/Header.jsx
+++ src/sections/layoutSections/Header.jsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 import styled from "styled-components";
 import LanSelect from "./headerSections/LanSelect";
 import { useDispatch, useSelector } from "react-redux";
+import MobileTab from "./headerSections/MobileTab";
 const HeaderStyle = styled.div`
   & .navs {
     border-bottom: 1px solid #235dff;
@@ -125,77 +126,80 @@ const Header = () => {
     }
   };
   return (
-    <HeaderStyle className="container_main">
-      <div className="ds_flex navs">
-        <div className=" left_nav ">
-          <Link to="/" className="logo">
-            <img src="/images/logo.svg" className="logo_desktop" />
-            <img src="/images/logoM.svg" className="logo_mobile" />
-          </Link>
-        </div>
-        <div className="ds_flex right_nav">
-          <div className="right_links">
-            <NavLink to={"/contacts"} className={"r_link"}>
-              Kонтакты
-            </NavLink>
-            <NavLink to={"/abaut"} className={"r_link"}>
-              О компании
-            </NavLink>
+    <>
+      <HeaderStyle className="container_main">
+        <div className="ds_flex navs">
+          <div className=" left_nav ">
+            <Link to="/" className="logo">
+              <img src="/images/logo.svg" className="logo_desktop" />
+              <img src="/images/logoM.svg" className="logo_mobile" />
+            </Link>
           </div>
-          <div className="lan">
-            <LanSelect />
-          </div>
-        </div>
-      </div>
-      <div className="ds_flex second_navs">
-        <div className="item_left">
-          <NavLink className="item first_item" to="/">
-            Главная
-          </NavLink>
-          <NavLink className="item" to="/agets">
-            Агенты
-          </NavLink>
-          <NavLink className="item" to="/my-applications">
-            Мои заявки
-          </NavLink>
-          <NavLink className="item" to="/travel-agencies">
-            Турфирмы
-          </NavLink>
-        </div>
-        <div className="dc_flex item_right">
-          {user?.id ? (
-            <>
-              <Link to="/profile" className="dc_flex user">
-                <div className="user_icon">
-                  <img src="/images/profile/user.svg" alt="tripusk user" />
-                </div>
-                <div className="user_name">
-                  <div className="top_text">Good morning! 🌤️</div>
-                  <div className="name">{showUserName()}</div>
-                </div>
-              </Link>
-              <Link to="/notification" className="dc_flex user_notification">
-                <img
-                  src="/images/profile/notificationHas.svg"
-                  alt="notification user"
-                />
-              </Link>
-            </>
-          ) : (
-            <div className="dc_flex user">
-              <div
-                className="user_name"
-                onClick={() => {
-                  dispatch({ type: "SET_AUTH_MODAL", payload: true });
-                }}
-              >
-                <div className="name">Войти</div>
-              </div>
+          <div className="ds_flex right_nav">
+            <div className="right_links">
+              <NavLink to={"/contacts"} className={"r_link"}>
+                Kонтакты
+              </NavLink>
+              <NavLink to={"/abaut"} className={"r_link"}>
+                О компании
+              </NavLink>
             </div>
-          )}
+            <div className="lan">
+              <LanSelect />
+            </div>
+          </div>
         </div>
-      </div>
-    </HeaderStyle>
+        <div className="ds_flex second_navs">
+          <div className="item_left">
+            <NavLink className="item first_item" to="/">
+              Главная
+            </NavLink>
+            <NavLink className="item" to="/agets">
+              Агенты
+            </NavLink>
+            <NavLink className="item" to="/my-applications">
+              Мои заявки
+            </NavLink>
+            <NavLink className="item" to="/travel-agencies">
+              Турфирмы
+            </NavLink>
+          </div>
+          <div className="dc_flex item_right">
+            {user?.id ? (
+              <>
+                <Link to="/profile" className="dc_flex user">
+                  <div className="user_icon">
+                    <img src="/images/profile/user.svg" alt="tripusk user" />
+                  </div>
+                  <div className="user_name">
+                    <div className="top_text">Good morning! 🌤️</div>
+                    <div className="name">{showUserName()}</div>
+                  </div>
+                </Link>
+                <Link to="/notification" className="dc_flex user_notification">
+                  <img
+                    src="/images/profile/notificationHas.svg"
+                    alt="notification user"
+                  />
+                </Link>
+              </>
+            ) : (
+              <div className="dc_flex user">
+                <div
+                  className="user_name"
+                  onClick={() => {
+                    dispatch({ type: "SET_AUTH_MODAL", payload: true });
+                  }}
+                >
+                  <div className="name">Войти</div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </HeaderStyle>
+      <MobileTab />
+    </>
   );
 };
 
---
+++ src/sections/layoutSections/headerSections/MobileTab.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import {
+  accountIcon,
+  favoriteIcon,
+  homeIcon,
+  turIcon,
+} from "../../../assets/headIcons";
+const MobileTabStyle = styled.div`
+  display: none;
+  height: 60px;
+  background-color: #fff;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  z-index: 1000;
+  border-top: 0.5px solid #235dff;
+  justify-content: space-around;
+  @media (max-width: 900px) {
+    display: flex;
+  }
+  & .link {
+    & .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    & .title {
+      color: #9e9e9e;
+      font-size: 10px;
+      font-weight: 500;
+      line-height: 16px;
+      letter-spacing: 0.2px;
+      text-align: center;
+    }
+  }
+`;
+const MobileTab = () => {
+  return (
+    <MobileTabStyle className="ds_flex">
+      <NavLink className="link" to="#">
+        <div className="icon">{homeIcon}</div>
+        <div className="title">Главная</div>
+      </NavLink>
+      {/* <NavLink className='link' to="#">
+        <div className="icon">{favoriteIcon}</div>
+        <div className="title">Избранные</div>
+      </NavLink> */}
+      {/* <NavLink className='link' to="#">
+        <div className="icon">{turIcon}</div>
+        <div className="title">Горящие туры</div>
+      </NavLink> */}
+      <NavLink className="link" to="#">
+        <div className="icon">{accountIcon}</div>
+        <div className="title">Аккаунт</div>
+      </NavLink>
+    </MobileTabStyle>
+  );
+};
+
+export default MobileTab;
