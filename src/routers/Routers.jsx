db4b659ebfed599374d34f6efa7b05f036657eed
kamol0007 <kamolbek00070007@gmail.com>
2024-04-05 10:13:37 +0500

    clear_code

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,76 +1,49 @@
-import React, { Suspense, lazy } from 'react';
-import { Outlet, Route, Routes } from 'react-router-dom';
-import AppView from '../components/profile_pages/AppView';
-import Layout from '../sections/layoutSections/Layout';
+import React, { Suspense, lazy } from "react";
+import { Outlet, Route, Routes } from "react-router-dom"; 
+import Layout from "../sections/layoutSections/Layout";
 
-const Home = lazy(() => import('../components/Home'));
-const Home2 = lazy(() => import('../components/Home2'));
-const MyApp = lazy(() => import('../components/profile_pages/MyApp'));
-const Profile = lazy(() => import('../components/profile_pages/Profile'));
-const NotFount = lazy(() => import('../components/NotFount'));
+const Home = lazy(() => import("../components/main_pages/Home"));
+const Profile = lazy(() => import("../components/profile_pages/Profile"));
+const NotFount = lazy(() => import("../components/main_pages/NotFount"));
 
 const Routers = () => {
-	return (
-		<Routes>
-			<Route
-				element={
-					<Layout>
-						<Outlet />
-					</Layout>
-				}
-			>
-				<Route path='/'>
-					<Route
-						index
-						element={
-							<Suspense>
-								<Home />
-							</Suspense>
-						}
-					/>
-					{/* <Route
-						path='/home2'
-						element={
-							<Suspense>
-								<Home2 />
-							</Suspense>
-						}
-					/> */}
-					<Route
-						path='/profile'
-						element={
-							<Suspense>
-								<Profile />
-							</Suspense>
-						}
-					/>
-					<Route
-						path='/my-apps'
-						element={
-							<Suspense>
-								<MyApp />
-							</Suspense>
-						}
-					/>
-					<Route
-						path='/my-apps/:id'
-						element={
-							<Suspense>
-								<AppView />
-							</Suspense>
-						}
-					/>
-				</Route>
-			</Route>
-			<Route
-				path='*'
-				element={
-					<Suspense>
-						<NotFount />
-					</Suspense>
-				}
-			/>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route
+        element={
+          <Layout>
+            <Outlet />
+          </Layout>
+        }
+      >
+        <Route path="/">
+          <Route
+            index
+            element={
+              <Suspense>
+                <Home />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <Suspense>
+                <Profile />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Route>
+      <Route
+        path="*"
+        element={
+          <Suspense>
+            <NotFount />
+          </Suspense>
+        }
+      />
+    </Routes>
+  );
 };
 export default Routers;
