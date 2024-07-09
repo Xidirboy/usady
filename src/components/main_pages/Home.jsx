3977916aba293bd5a1922e1f1f046d22b005a1ba
Xidirboy <122156062+Xidirboy@users.noreply.github.com>
2024-07-09 12:12:04 +0500

    Update Home.jsx

--- src/components/main_pages/Home.jsx
+++ src/components/main_pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Home = () => {
+  const {t} = useTranlation()
   return (
     <div className="container text-dodgerBlue">
-      Home
+      {t("home.title")}
       <p className="font-thin">Whereas recognition of the inherent dignity</p>
     </div>
   );
